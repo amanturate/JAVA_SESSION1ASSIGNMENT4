e87c242f4be8f25b1ef81c4df467b58462355cab
Aman Turate <amanturate@users.noreply.github.com>
2016-08-25 14:43:56 +0530

    Add files via upload

---
+++ ASSIGNMENT4.java
@@ -0,0 +1,13 @@
+
+public class ASSIGNMENT4 {
+
+	public static void main(String[] args) {
+		char ch;
+		int i;
+		
+		for(i=65; i<91; i++){
+			ch = (char)i;
+			System.out.println(i + " - " + ch);
+		}
+	}
+}
